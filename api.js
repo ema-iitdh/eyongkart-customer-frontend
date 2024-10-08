42ce36b109735b292edc5a8b7c04b49e22fa6338
ChakpramSupriya <chakpramsupriya@gmail.com>
2024-10-08 23:46:48 +0530

    changes in  ui

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "https://e-commerce-ten-rust.vercel.app",
+  baseURL: "http://139.59.24.128/api",
   // baseURL: "http://localhost:3000",
 });
 export default instance;
