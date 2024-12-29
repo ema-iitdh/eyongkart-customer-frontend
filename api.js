798b3b938e0a99a0c9804f062ad4d58bcefeb18b
nganaremba-rem <ngancloverr+remkugithub@gmail.com>
2024-12-29 14:42:21 +0530

    changed api

--- api.js
+++ api.js
@@ -1,8 +1,11 @@
-import axios from "axios";
+import axios from 'axios';
 
 // export const url = "https://eyongkart.com/api";
-export const url = "https://e-commerce-ten-rust.vercel.app";
+export const url = 'http://localhost:5000';
+// export const url = "https://e-commerce-ten-rust.vercel.app";
 // export const url = "https://qfqhhctz-5000.inc1.devtunnels.ms/";
 export const Axios = axios.create({
   baseURL: url,
+  // to send cookies with the request
+  withCredentials: true,
 });
