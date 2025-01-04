2f5ef398c53cf5ae197eca6ae651df90bc538feb
ChakpramSupriya <chakpramsupriya@gmail.com>
2025-01-04 14:28:04 +0530

    update logo

--- src/config/api.js
+++ src/config/api.js
@@ -4,20 +4,21 @@
 // export const url = "https://qfqhhctz-5000.inc1.devtunnels.ms/";
 
 export const API_CONFIG = {
-  BASE_URL: import.meta.env.VITE_API_BASE_URL || 'https://localhost:5000',
-  // BASE_URL: 'https://eyongkart.com/api',
+  // BASE_URL: import.meta.env.VITE_API_BASE_URL || 'https://localhost:5000',
+  BASE_URL: "https://eyongkart.com/api",
+  // BASE_URL: "https://0925-103-120-31-122.ngrok-free.app",
   TIMEOUT: 10000,
-  CREDENTIALS: 'include',
+  CREDENTIALS: "include",
   WITH_CREDENTIALS: true,
   HEADERS: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
-    'ngrok-skip-browser-warning': true,
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    "ngrok-skip-browser-warning": true,
   },
 };
 
 export const ERROR_MESSAGES = {
-  NETWORK_ERROR: 'Network Error. Please check your connection.',
-  SERVER_ERROR: 'Server Error. Please try again later.',
-  UNAUTHORIZED: 'Session expired. Please login again.',
+  NETWORK_ERROR: "Network Error. Please check your connection.",
+  SERVER_ERROR: "Server Error. Please try again later.",
+  UNAUTHORIZED: "Session expired. Please login again.",
 };
